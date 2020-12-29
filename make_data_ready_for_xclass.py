6283679c5db193c554a928384fc016751281d840
dheeraj7596 <dheeraj7596.dm@gmail.com>
2020-12-28 21:23:50 -0800

    Making data ready for xclass

---
+++ make_data_ready_for_xclass.py
@@ -0,0 +1,30 @@
+import pickle
+
+if __name__ == "__main__":
+    basepath = "/Users/dheerajmekala/Work/Coarse2Fine/data/nyt/"
+    dataset = "business/"
+    pkl_dump_dir = basepath
+    xclass_dump_dir = "/Users/dheerajmekala/Work/XClass/data/datasets/" + dataset
+
+    df = pickle.load(open(pkl_dump_dir + "df_fine.pkl", "rb"))
+    parent_to_child = pickle.load(open(pkl_dump_dir + "parent_to_child.pkl", "rb"))
+    classes = parent_to_child["business"]
+
+    df = df[df.label.isin(classes)].reset_index(drop=True)
+
+    f1 = open(xclass_dump_dir + "classes.txt", "w")
+    f2 = open(xclass_dump_dir + "labels.txt", "w")
+    f3 = open(xclass_dump_dir + "dataset.txt", "w")
+
+    label_to_idx = {}
+    for i, l in enumerate(classes):
+        label_to_idx[l] = i
+        f1.write(l + "\n")
+
+    for i, row in df.iterrows():
+        f2.write(str(label_to_idx[row["label"]]) + "\n")
+        f3.write(row["text"] + "\n")
+
+    f1.close()
+    f2.close()
+    f3.close()
